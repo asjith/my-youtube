41da7d63d3ba9c44f32d00fa6252e0dd2eb89311
anusreesjith <anusreesjith@gmail.com>
2025-11-22 16:52:23 +0530

    horizontal button movement using transform

--- src/components/ButtonList.js
+++ src/components/ButtonList.js
@@ -1,28 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { BUTTONLIST_BUTTONS } from "../utils/constants";
 
 const ButtonList = () => {
+  const [showBtn, setShowBtn] = useState({ left: false, right: true });
+
+  const handleRightArrowClick = () => {
+    setShowBtn({ left: true, right: true });
+  };
+
   return (
     <div className="grid grid-flow-col items-center m-4 relative">
-      <div className="bg-white col-span-2 absolute left-0 z-10 ">
-        <button className="border border-solid border-black rounded-full w-8 h-8 bg-white">
-          <strong>&lt;</strong>
-        </button>
+      {showBtn.left && (
+        <div className="col-span-2 absolute left-0 z-10 bg-white rounded-r-full">
+          <button className="border border-solid border-black rounded-full w-8 h-8 bg-white">
+            <strong>&lt;</strong>
+          </button>
+        </div>
+      )}
+      <div className="col-span-8 overflow-hidden">
+        <div className="slidder flex flex-nowrap gap-2">
+          {BUTTONLIST_BUTTONS.map((buttonInfo) => {
+            return (
+              <button
+                key={buttonInfo.id}
+                className="text-xs px-2 py-1 bg-gray-200 rounded-md"
+              >
+                {buttonInfo.name}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <div className="col-span-8 flex flex-nowrap gap-2 overflow-hidden">
-        {BUTTONLIST_BUTTONS.map((buttonInfo) => {
-          return (
-            <button
-              key={buttonInfo.id}
-              className="text-xs px-2 py-1 bg-gray-200 rounded-md"
-            >
-              {buttonInfo.name}
-            </button>
-          );
-        })}
-      </div>
-      <div className="bg-white col-span-2 absolute right-0 z-10 ">
-        <button className="border border-solid border-black rounded-full w-8 h-8 bg-white">
+      <div className="col-span-2 absolute right-0 z-10 bg-white rounded-l-full">
+        <button
+          className="border border-solid border-black rounded-full w-8 h-8 bg-white"
+          onClick={handleRightArrowClick}
+        >
           <strong>&gt;</strong>
         </button>
       </div>
