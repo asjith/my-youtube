4afd317806373889187decc8b452e57a86c87be6
anusreesjith <anusreesjith@gmail.com>
2025-11-30 23:44:55 +0530

    using youtube api

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,6 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { YOUTUBE_API } from "../utils/constants";
 
 const VideoList = () => {
+  useEffect(() => {
+    fetchYoutubeVideoDatas();
+  }, []);
+
+  const fetchYoutubeVideoDatas = async () => {
+    const data = await fetch(YOUTUBE_API);
+    const json = await data.json();
+    console.log(json);
+  };
   return <div>VideoList</div>;
 };
 
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -119,3 +119,7 @@ export const BUTTONLIST_BUTTONS = [
     name: "Gadgets",
   },
 ];
+
+export const YOUTUBE_API =
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&key=" +
+  process.env.REACT_APP_YOUTUBE_API_KEY;
