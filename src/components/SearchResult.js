bcfd0ebc6e4f45d3d5c5f86327663c2dec8c66df
anusreesjith <anusreesjith@gmail.com>
2025-12-05 20:11:52 +0530

    rendering video cards in search result page

--- src/components/SearchResult.js
+++ src/components/SearchResult.js
@@ -1,8 +1,10 @@
-import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import { YOUTUBE_SEARCH_RESULTS_API } from "../utils/constants";
+import VideoCard from "./VideoCard";
 
 const SearchResult = () => {
+  const [searchResults, setSearchResults] = useState([]);
   const [searchParams] = useSearchParams();
 
   useEffect(() => {
@@ -14,10 +16,19 @@ const SearchResult = () => {
       YOUTUBE_SEARCH_RESULTS_API + searchParams.get("search_query")
     );
     const json = await data.json();
+    setSearchResults(json.items);
     console.log(json.items);
   };
 
-  return <div>SearchResult</div>;
+  return (
+    <div className="m-4 grid  gap-2 grid-cols-1">
+      {searchResults.map((result) => (
+        <Link to={"/watch?v=" + result.id.videoId} key={result.id.videoId}>
+          <VideoCard info={result} calledFrom="SearchResult" />
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default SearchResult;
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { CHANNEL_API } from "../utils/constants";
 
-const VideoCard = ({ info }) => {
+const VideoCard = ({ info, calledFrom }) => {
   const [channelInfo, setChannelInfo] = useState(null);
 
   const { snippet, statistics } = info;
@@ -18,11 +18,11 @@ const VideoCard = ({ info }) => {
   };
 
   const views =
-    statistics.viewCount / 1000000 < 1
-      ? statistics.viewCount / 1000 < 1
-        ? statistics.viewCount
-        : Math.trunc(statistics.viewCount / 1000) + "K "
-      : Math.trunc(statistics.viewCount / 1000000) + "M ";
+    statistics?.viewCount / 1000000 < 1
+      ? statistics?.viewCount / 1000 < 1
+        ? statistics?.viewCount
+        : Math.trunc(statistics?.viewCount / 1000) + "K "
+      : Math.trunc(statistics?.viewCount / 1000000) + "M ";
 
   const getDuration = (publishedAt) => {
     const today = new Date();
@@ -58,16 +58,26 @@ const VideoCard = ({ info }) => {
   const duration = getDuration(publishedAt);
 
   return (
-    <div className="p-1 cursor-pointer shadow-md rounded-md hover:bg-red-100">
+    <div
+      className={`p-1 cursor-pointer shadow-md rounded-md hover:bg-red-100 ${
+        calledFrom == "SearchResult" && "grid grid-cols-12 gap-2"
+      }`}
+    >
       <img
-        className="rounded-md w-full"
+        className={`rounded-md w-full ${
+          calledFrom == "SearchResult" && "col-span-4"
+        }`}
         src={thumbnails.medium.url}
         alt="thumbnail"
       />
-      <div className="flex gap-2 py-2">
+      <div
+        className={`flex gap-2 py-2 ${
+          calledFrom == "SearchResult" && "col-span-8"
+        }`}
+      >
         {channelInfo && (
           <img
-            className="rounded-full w-10 h-10"
+            className="rounded-full w-5 h-5 md:w-10 md:h-10"
             src={channelInfo.thumbnails.default.url}
             alt="channel-profile-pic"
           />
@@ -77,7 +87,12 @@ const VideoCard = ({ info }) => {
           <div className="text-xs text-gray-500 py-1">
             <p>{channelTitle}</p>
             <p>
-              <span>{views} views</span> . <span>{duration}</span>
+              {!isNaN(views) && (
+                <>
+                  <span>{views} views</span> .
+                </>
+              )}
+              <span>{duration}</span>
             </p>
           </div>
         </div>
