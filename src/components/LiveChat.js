3ed673dd714bd236392181ed82eee79de0ac2785
anusreesjith <anusreesjith@gmail.com>
2025-12-13 18:00:59 +0530

    design live chat container

---
+++ src/components/LiveChat.js
@@ -0,0 +1,9 @@
+const LiveChat = () => {
+  return (
+    <div className="border border-black rounded-md w-full aspect-video my-2">
+      LiveChat
+    </div>
+  );
+};
+
+export default LiveChat;
--- src/components/Watch.js
+++ src/components/Watch.js
@@ -4,6 +4,7 @@ import { closeSideBar } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
 import Comment from "./Comment";
 import { COMMENTS_DATA } from "../utils/constants";
+import LiveChat from "./LiveChat";
 
 const Watch = () => {
   const [searchParams] = useSearchParams();
@@ -16,23 +17,29 @@ const Watch = () => {
 
   return (
     <div className="col-span-9 m-2 p-2">
-      <iframe
-        className="rounded-md w-full aspect-video my-2 md:w-4/6"
-        src={
-          "https://www.youtube.com/embed/" +
-          searchParams.get("v") +
-          "?si=Wei6g8TNvvsJM8ym&autoplay=1"
-        }
-        title="YouTube video player"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+      <div className="md:flex gap-2">
+        <iframe
+          className="rounded-md w-full aspect-video my-2 md:w-4/6"
+          src={
+            "https://www.youtube.com/embed/" +
+            searchParams.get("v") +
+            "?si=Wei6g8TNvvsJM8ym&autoplay=1"
+          }
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <LiveChat />
+      </div>
       <div className="w-full md:w-4/6">
-        {COMMENTS_DATA.map((comment) => (
-          <Comment info={comment} />
-        ))}
+        <h1 className="font-bold">Comments:</h1>
+        <div>
+          {COMMENTS_DATA.map((comment) => (
+            <Comment info={comment} />
+          ))}
+        </div>
       </div>
     </div>
   );
