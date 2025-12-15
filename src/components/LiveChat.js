b83f36d0afd1a980d96489e541541d0f369f4c29
anusreesjith <anusreesjith@gmail.com>
2025-12-15 17:11:19 +0530

    using openai api to generate random chat messages

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -2,6 +2,7 @@ import { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
+import openai from "../utils/openai";
 
 const LiveChat = () => {
   const chatId = useRef(0);
@@ -10,14 +11,20 @@ const LiveChat = () => {
 
   useEffect(() => {
     //API polling
-    const i = setInterval(() => {
-      console.log("API polling");
+    const i = setInterval(async () => {
+      const response = await openai.responses.create({
+        model: "gpt-5-nano",
+        input:
+          "I am trying to build a live chat section in my front-end YouTube project. I want you to generate an object where it contains a key named name(with quotes) with value as a name of the user, some random name can be used and the object should also contain a message key(with quotes) with value as a message which might contain maximum words of 10 or less than 10. Send the object alone, no other writings. Return a valid JSON",
+      });
+
+      const responseObject = JSON.parse(response.output_text);
 
       dispatch(
         addMessage({
           id: chatId.current++,
-          name: "User" + chatId.current,
-          message: "Wow this is live Chat!!",
+          name: responseObject.name,
+          message: responseObject.message,
         })
       );
     }, 2000);
