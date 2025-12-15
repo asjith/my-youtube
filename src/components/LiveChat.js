83919a1c3e7fcacc2741f149feddf9dab6958565
anusreesjith <anusreesjith@gmail.com>
2025-12-15 11:50:23 +0530

    implementation of API polling

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,6 +1,16 @@
+import { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+  useEffect(() => {
+    //API polling
+    const i = setInterval(() => {
+      console.log("API polling");
+    }, 2000);
+
+    return () => clearInterval(i);
+  }, []);
+
   return (
     <div className="border border-gray-400 rounded-md w-full aspect-video my-2">
       <ChatMessage name="User 1" message="This is live Chat" />
