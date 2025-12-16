73b93e2894975f4949e4969202a9cab2f7792edd
anusreesjith <anusreesjith@gmail.com>
2025-12-16 12:22:54 +0530

    calling openai api using recursive setTimeout

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,42 +1,52 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
 import { useDispatch, useSelector } from "react-redux";
 import { addMessage } from "../utils/chatSlice";
 import openai from "../utils/openai";
 
 const LiveChat = () => {
-  const [predefinedMessages, setPredefinedMessages] = useState([]);
+  const messageQueue = useRef([]);
   const chatId = useRef(0);
+  const timeoutRef = useRef(null);
+  const isUnmounted = useRef(false);
   const dispatch = useDispatch();
   const chatMessage = useSelector((store) => store.chat);
 
   useEffect(() => {
-    fetchRandomMessages();
-  }, []);
+    fetchLiveMessages();
 
-  useEffect(() => {
-    if (predefinedMessages.length > 0) {
-      //API polling
-      const i = setInterval(() => {
+    const i = setInterval(() => {
+      if (messageQueue.current.length > 0) {
         dispatch(
           addMessage({
             id: ++chatId.current,
-            ...predefinedMessages[Math.floor(Math.random() * 50)],
+            ...messageQueue.current.shift(),
           })
         );
-      }, 1000);
+      }
+    }, 1000);
 
-      return () => clearInterval(i);
-    }
-  }, [predefinedMessages]);
+    return () => {
+      isUnmounted.current = true;
+      clearInterval(i);
+      if (timeoutRef.current) clearTimeout(timeoutRef.current);
+    };
+  }, []);
 
-  const fetchRandomMessages = async () => {
+  const fetchLiveMessages = async () => {
+    if (isUnmounted.current) return;
+
     const response = await openai.responses.create({
       model: "gpt-5-nano",
       input:
-        "I am trying to build a live chat section in my front-end YouTube project. I want you to generate an array of 50 objects where each object contains a key named name(with quotes) with value as a name of the user, some random name can be used and the object should also contain a message key(with quotes) with value as a message which might contain maximum words of 10 or less than 10. The message can be about how awesome the video is, greetings, with or without emojis. Send the object alone, no other writings. Return a valid JSON",
+        "I am trying to build a live chat section in my front-end YouTube project. I want you to generate an array of 3 objects where each object contains a key named name(with quotes) with value as a name of the user, some random name can be used and the object should also contain a message key(with quotes) with value as a message which might contain maximum words of 10 or less than 10. The message can be about how awesome the video is, greetings, with or without emojis. Send the object alone, no other writings. Return a valid JSON",
     });
-    setPredefinedMessages(JSON.parse(response.output_text));
+
+    if (isUnmounted.current) return;
+
+    messageQueue.current.push(...JSON.parse(response.output_text));
+
+    timeoutRef.current = setTimeout(fetchLiveMessages, 1000);
   };
 
   return (
