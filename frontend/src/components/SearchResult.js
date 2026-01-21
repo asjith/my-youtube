ec89effaba2d03dd87022f045d7ae302474f14b0
anusreesjith <anusreesjith@gmail.com>
2026-01-21 17:31:31 +0530

    feat: using useQuery to fetch search results

--- frontend/src/components/SearchResult.js
+++ frontend/src/components/SearchResult.js
@@ -1,92 +1,58 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef } from "react";
 import { Link, useSearchParams } from "react-router-dom";
-import { YOUTUBE_SEARCH_RESULTS_API } from "../utils/constants";
+
 import VideoCard from "./VideoCard";
 import Offline from "./Offline";
 import { useSelector } from "react-redux";
 import Loading from "./Loading";
 import NoResultsFound from "./NoResultsFound";
-import { checkOfflineError } from "../utils/helperFunctions";
+
 import Error from "./Error";
+import { useQuery } from "@tanstack/react-query";
+import { fetchSearchResults } from "../utils/api";
 
 const SearchResult = () => {
-  const [searchResults, setSearchResults] = useState({
-    search: "",
-    result: [],
-  });
-  const [displayOffline, setDisplayOffline] = useState(false);
-  const [retryCount, setRetryCount] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
-  const throttleFlag = useRef(true);
   const [searchParams] = useSearchParams();
   const isOnline = useSelector((store) => store.app.isOnline);
+  const prevIsOnline = useRef(isOnline);
+
+  const {
+    data: searchResults,
+    isLoading,
+    isError,
+    error,
+    refetch
+  } = useQuery({
+    queryKey: ["search", searchParams.get("search_query")],
+    queryFn: () => fetchSearchResults(searchParams.get("search_query")),
+    retry: 0,
+    staleTime: Infinity,
+    gcTime: 1000 * 60 * 5,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false
+  });
 
   useEffect(() => {
-    if (
-      searchResults.result.length > 0 &&
-      searchResults.search === searchParams
-    )
-      return;
-    fetchSearchResults();
-  }, [searchParams, retryCount, isOnline]);
+    if (!prevIsOnline.current && isOnline) refetch();
+    prevIsOnline.current = isOnline;
+  }, [isOnline]);
 
-  const fetchSearchResults = async () => {
-    setLoading(true);
-    setError(false);
-    setDisplayOffline(false);
-    try {
-      const data = await fetch(
-        YOUTUBE_SEARCH_RESULTS_API + searchParams.get("search_query")
-      );
-      if (!data.ok) {
-        console.error("HTTP Error", {
-          status: data.status,
-          statusText: data.statusText,
-          url: data.url,
-          timestamp: new Date().toISOString(),
-        });
-        setError(true);
-        setSearchResults({ search: "", result: [] });
-      } else {
-        const json = await data.json();
-        setSearchResults({ search: searchParams, result: json.items });
-      }
-    } catch (error) {
-      //network error
-      if (!navigator.onLine || checkOfflineError(error.message))
-        setDisplayOffline(true);
-      else {
-        console.error("Network Error", error);
-        setError(true);
-        setSearchResults({ search: "", result: [] });
-      }
+  if (isLoading) return <Loading />;
+
+  if (isError) {
+    if (!navigator.onLine) return <Offline refetch={refetch} />;
+    else if (error.message.includes("404")) return <NoResultsFound />;
+    else {
+      console.error(error);
+      return <Error refetch={refetch} />;
     }
-    setLoading(false);
-  };
+  }
 
-  const handleRetry = () => {
-    //throttling
-    if (throttleFlag.current) {
-      setRetryCount((rc) => rc + 1);
-      throttleFlag.current = false;
-      setTimeout(() => {
-        throttleFlag.current = true;
-      }, 3000);
-    }
-  };
-
-  if (loading) return <Loading />;
-
-  if (displayOffline) return <Offline onClick={handleRetry} />;
-
-  if (error) return <Error onClick={handleRetry} />;
-
-  if (searchResults.result.length === 0) return <NoResultsFound />;
+  if (searchResults?.length === 0) return <NoResultsFound />;
 
   return (
     <div className="m-4 grid  gap-2 grid-cols-1">
-      {searchResults.result.map((result) => (
+      {searchResults?.map((result) => (
         <Link to={"/watch?v=" + result.id.videoId} key={result.id.videoId}>
           <VideoCard info={result} calledFrom="SearchResult" />
         </Link>
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,4 +1,8 @@
-import { CHANNEL_API, YOUTUBE_API } from "./constants";
+import {
+  CHANNEL_API,
+  YOUTUBE_API,
+  YOUTUBE_SEARCH_RESULTS_API
+} from "./constants";
 
 export const fetchYoutubeVideos = async () => {
   const data = await fetch(YOUTUBE_API);
@@ -24,4 +28,17 @@ export const fetchYoutubeChannelDetails = async (channelId) => {
   const json = await data.json();
 
   return json.items[0].snippet;
+};
+
+export const fetchSearchResults = async (search) => {
+  const data = await fetch(YOUTUBE_SEARCH_RESULTS_API + search);
+
+  if (!data.ok)
+    throw new Error(
+      `HTTP error, ${data.status} ${data.statusText} at ${data.url} (${new Date().toISOString()})`
+    );
+
+  const json = await data.json();
+
+  return json.items;
 };
